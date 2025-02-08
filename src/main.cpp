f362fe5ec3906a4b71be6fdb831287dc6f6b2875
Shakkhat Zaman <shakkhatzaman@gmail.com>
2025-02-08 22:45:08 +0600

    Added headers and resources directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -17,11 +22,16 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
     gladLoadGL();
 
-    /* Loop until the user closes the window */
+    std::cout << RESOURCES_DIR << '\n';
+    std::cout << glGetString(GL_VENDOR) << '\n';
+    std::cout << glGetString(GL_RENDERER) << '\n';
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    stbi_set_flip_vertically_on_load(true);
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
